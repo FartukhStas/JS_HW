bb707d76adca0edd4621ec7d2f49b3fb0f882191
Stas <fartuhstas@gmail.com>
2023-01-07 14:58:48 +0200

    second commit

---
+++ 2211_nov_2022_JS-main/03-lesson. arrays/sandbox/02_objects.js
@@ -0,0 +1,38 @@
+// [] - array
+// {} - object (hash, ассоциативний массів, словарь)
+
+// let users = ["Sveta", "Olga", "Ivan", "John"];
+// console.log(users);
+// console.log(typeof users);
+// console.log(Array.isArray(users));
+
+// let sveta = {
+//     name: "Sveta",
+//     age: 25,
+// };
+
+// console.log(sveta.age);
+// console.log(sveta["age"]);
+// console.log(sveta.name);
+// console.log(sveta["name"]);
+
+// console.log(sveta);
+// console.log(typeof sveta);
+// console.log(Array.isArray(sveta));
+
+// const friends = [
+//     {
+//         name: "Sveta",
+//         age: 25,
+//     },
+//     {
+//         name: "Bob",
+//         age: 30,
+//     },
+//     {
+//         name: "Mike",
+//         age: 18,
+//     },
+// ];
+
+// console.log(friends);
---
+++ 2211_nov_2022_JS-main/04-lesson. functions/08-scope.js
@@ -0,0 +1,15 @@
+// Область видимости
+//  {}
+
+// Global scope
+
+// let a = 10;
+
+// function get(a) {
+//     // let qwe = 20;
+//     // console.log(qwe);
+
+//     console.log(a);
+// }
+
+// get(30);
